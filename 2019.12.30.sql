eda951da1c19b3843bdc7ea21a86154e4f9673cd
1jobam <limm2770@naver.com>
2020-01-09 08:59:49 +0900

    2019.12.30

---
+++ 2019.12.30.sql
@@ -0,0 +1,16 @@
+-- mybatis 사용
+-- 1. 공식 사이트 참조
+-- 2. 구글(다른 사람의 블로그)
+
+-- mybatis 홈페이지 참조하여
+-- https://mybatis.org/mybatis-3/ko/getting-started.html
+
+-- ORACLE DB 연동
+-- final	String	driver		=	"oracle.jdbc.driver.OracleDriver";
+-- final	String	url			=	"jdbc:oracle:thin:@127.0.0.1:1521:orcl";
+-- final	String	user		=	"pc01";
+-- final	String	pass		=	"java";
+
+--mybatis 맵퍼 파일을 만들어서 맵핑을 해준다.
+
+--mybatis 홈페이지 시작하기 진행
